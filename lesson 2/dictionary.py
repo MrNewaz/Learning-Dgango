f87ea0952f53fc52c0c80f4e73ab927c63cf71f3
MrNewaz <saifur.saif@northsouth.edu>
2021-07-04 20:26:43 +0600

    dicktonary

---
+++ lesson 2/dictionary.py
@@ -0,0 +1,13 @@
+person = {
+    "key": "value",
+    "name": "Saif",
+    "insta": '@saif.newaz'
+}
+
+print(person['name'])
+
+person['facebook'] = '@saif.facebook'
+
+del person['key']
+
+print(person)
---
+++ lesson 2/lists.py
@@ -0,0 +1,8 @@
+lst = ['String', 12, ['New Item'], 'Saif']
+
+lst.append('Newaz')
+lst.remove(12)
+lst.sort()
+
+for item in lst:
+    print('The item is: ', item)
